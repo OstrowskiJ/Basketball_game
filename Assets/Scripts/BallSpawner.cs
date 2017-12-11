67941dd07ae2206c52a283f6a83ab854fac869da
Jakub Ostrowski <jakub.ostrowski1404@gmail.com>
2017-12-11 22:54:54 +0100

    Added scripts of rotating camera and creation balls

---
+++ Assets/Scripts/BallSpawner.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallSpawner : MonoBehaviour {
+
+	public GameObject ballPrefab;
+
+	// Use this for initialization
+	void Start ()
+	{
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (Input.GetKeyDown(KeyCode.LeftArrow))
+		{
+			GameObject instance = Instantiate(ballPrefab);
+			Rigidbody rb = instance.GetComponent<Rigidbody>();
+
+			rb.velocity = Vector3.left;
+		}
+
+		if (Input.GetKeyDown(KeyCode.RightArrow))
+		{
+			GameObject instance = Instantiate(ballPrefab);
+			Rigidbody rb = instance.GetComponent<Rigidbody>();
+
+			rb.velocity = Vector3.right;
+		}
+
+		if (Input.GetKeyDown(KeyCode.UpArrow))
+		{
+			GameObject instance = Instantiate(ballPrefab);
+			Rigidbody rb = instance.GetComponent<Rigidbody>();
+
+			rb.velocity = Vector3.forward;
+		}
+	}
+}
